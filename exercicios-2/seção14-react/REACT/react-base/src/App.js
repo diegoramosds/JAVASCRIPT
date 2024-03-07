926b247ae9cc54ec0d654ae288d38cf5a04b25b5
Diego <diegoramos3709@gmail.com>
2024-03-07 08:48:11 -0300

    aplicação do header

--- exercicios-2/seção14-react/REACT/react-base/src/App.js
+++ exercicios-2/seção14-react/REACT/react-base/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Login from './pages/login';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Header from './components/Header';
 
 function App() {
-  return <h1>Helo world</h1>;
+  return (
+    <>
+      <Header />
+      <Login />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
