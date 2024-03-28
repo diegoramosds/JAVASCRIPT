2417173066802091ee83d133c0fd53df2c449886
Diego <diegoramos3709@gmail.com>
2024-03-28 14:04:54 -0300

    Configurando edição

--- exercicios-2/seção14-react/REACT/consumir-api/src/pages/Alunos/index.js
+++ exercicios-2/seção14-react/REACT/consumir-api/src/pages/Alunos/index.js
@@ -11,17 +11,20 @@ import Loading from '../../components/Loading';
 
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
+      setIsLoading(true);
       const response = await axios.get('/alunos');
       setAlunos(response.data);
+      setIsLoading(false);
     }
     getData();
   }, []);
   return (
     <Container>
-      <Loading isLoading />
+      <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
 
       <AlunoContainer>
