59c1408d2d4cbc919c2152a5aba7238c0e69ac31
Diego <diegoramos3709@gmail.com>
2024-05-22 11:24:04 -0300

    iniciando projeto

---
+++ react/exercicios/8_REACT_ROUTER/react-router/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  //const [products, setProducts] = useState(data.products)
+
+
+  return (
+    <>
+      <div>
+       <h1>React router</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
