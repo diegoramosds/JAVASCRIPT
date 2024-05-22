e4b2c580b7635a12a240d429d1c2a36155937355
Diego <diegoramos3709@gmail.com>
2024-05-22 14:46:58 -0300

    opção de adicionar e excluir

--- react/exercicios/praticando/ex01/src/App.jsx
+++ react/exercicios/praticando/ex01/src/App.jsx
@@ -1,15 +1,61 @@
-import { useState } from 'react'
+import {useState, useEffect } from 'react'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
+
 
 import data from '../data/db.json'
 
 function App() {
   const [products, setProducts] = useState(data.products)
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+
+
+
   const handleDelete = (id) => {
     const updatedProducts = products.filter(product => product.id !== id);
     setProducts(updatedProducts);
+
+    localStorage.setItem("products", JSON.stringify(updatedProducts))
   }
+
+  useEffect(() => {
+    
+  },[])
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if (!name.trim() || !price.trim()) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+  
+  const newProduct = {
+    id: uuidv4(),
+    name,
+    price: parseFloat(price),
+  };
+
+  setProducts([...products, newProduct])
+
+  localStorage.setItem("products", JSON.stringify([...products, newProduct]))
+
+  setName("")
+  setPrice("")
+}
+
+ useEffect(() => {
+    const savedProducts = localStorage.getItem("products");
+    if (savedProducts) {
+       setProducts(JSON.parse(savedProducts))
+    }
+   
+  },[])
+
 
   return (
     <>
@@ -28,6 +74,21 @@ function App() {
       )}
 
         </ul>
+        <div className='add-product'>
+      <form>
+        <label>Name:
+        <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+      
+
+        <label>Price:
+        <input type="text" name='name' value={price} onChange={(e) => setPrice(e.target.value)}/>
+        </label>
+        
+
+       <input onClick={(e) => handleSubmit(e)} type="submit" value="Adicionar" />
+       </form>
+       </div>
       </div>
     </>
   );
