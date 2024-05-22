7555975ba0bbfae69041772b3b5658f940668810
Diego <diegoramos3709@gmail.com>
2024-05-22 09:16:21 -0300

    finalização do projeto

--- react/exercicios/7_REQ_HTTP/httpreact/src/App.js
+++ react/exercicios/7_REQ_HTTP/httpreact/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   //4 CUSTOM
-  const { data: items, httpConfig } = useFetch(url);
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
 
   const [name, setName] = useState("");
@@ -55,14 +55,26 @@ const handleSubmit = async (e) => {
   setPrice("")
 }
 
+// 8- desafio 6
+const handleRemove = (id) => {
+  httpConfig(id, "DELETE")
+}
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      <ul>
+      {/* 6- loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!loading && (
+        <ul>
         {items && items.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+          <li key={product.id}>{product.name} - R$: {product.price} <button onClick={() => handleRemove(product.id)}>Excluir</button></li>
         ) )}
       </ul>
+      )
+      }
+      
       <div className='add-product'>
         <form onSubmit={handleSubmit}>
           <label>
@@ -73,7 +85,10 @@ const handleSubmit = async (e) => {
             Preço:
             <input type="text" value={price} name='price' onChange={(e) => setPrice(e.target.value)}/>
           </label>
-          <input type="submit" value="Criar" />
+          {/* 7- state de loading no post */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar" />}
+          
         </form>
       </div>
     </div>
