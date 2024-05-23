773cd78570569a464a37c2ff7d4b7b852b2f2658
Diego <diegoramos3709@gmail.com>
2024-05-23 13:37:35 -0300

    implementando as rotas

--- react/exercicios/8_REACT_ROUTER/react-router/src/App.jsx
+++ react/exercicios/8_REACT_ROUTER/react-router/src/App.jsx
@@ -1,15 +1,26 @@
-// import { useState } from 'react'
+
 import './App.css'
+
+// 1
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
 
 
 function App() {
-  //const [products, setProducts] = useState(data.products)
-
-
   return (
     <>
-      <div>
+      <div className='App'>
        <h1>React router</h1>
+       <Link to={<About/>}>OI</Link>
+       <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+       </Routes>
+       </BrowserRouter>
       </div>
     </>
   );
