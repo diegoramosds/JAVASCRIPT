94aa20172570c6439f47ec3c41ed9b6aad543681
Diego <diegoramos3709@gmail.com>
2024-05-28 16:34:47 -0300

    configurando navigate

--- react/exercicios/8_REACT_ROUTER/react-router/src/App.jsx
+++ react/exercicios/8_REACT_ROUTER/react-router/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 
 // 1
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 //pages
 import Home from './pages/Home';
@@ -11,6 +11,8 @@ import NavBar from './components/NavBar';
 import Products from './pages/Products';
 import Info from './pages/Info';
 import Page404 from './pages/Page404';
+import SearchForm from './components/SearchForm';
+import Search from './pages/Search';
 
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
        <BrowserRouter>
        {/* 2- links com react router */}
         <NavBar/>
+        {/* 9- Search*/}
+        <SearchForm/>
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
@@ -28,14 +32,19 @@ function App() {
               <Route path='/products/:id/info' element={<Info />} />
               {/*4- rota dinamica */}
               <Route path='/products/:id' element={<Products />} />
+              {/* 9- search */}
+              <Route path='/search' element={<Search />} />
+              {/* 10- redirect */}
+              <Route path='/company' element={<Navigate to="/about" />} />
               {/* 7- */}
               <Route path='*' element={<Page404 />} />
+              
 
           </Routes>
        </BrowserRouter>
       </div>
     </>
   );
-}
+}  
 
 export default App
--- react/exercicios/8_REACT_ROUTER/react-router/src/components/NavBar.jsx
+++ react/exercicios/8_REACT_ROUTER/react-router/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import "./NavBar.css"
-import { Link, NavLink  } from "react-router-dom"
+import { NavLink  } from "react-router-dom"
 const NavBar = () => {
   return (
     <nav>
