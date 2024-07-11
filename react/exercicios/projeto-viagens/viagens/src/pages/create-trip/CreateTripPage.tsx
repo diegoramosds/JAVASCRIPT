3bc1a8dcf8394f727f2f127c634ce04f9080d3ef
Diego <diegoramos3709@gmail.com>
2024-07-11 12:03:54 -0300

    estilizando data

--- react/exercicios/projeto-viagens/viagens/src/pages/create-trip/CreateTripPage.tsx
+++ react/exercicios/projeto-viagens/viagens/src/pages/create-trip/CreateTripPage.tsx
@@ -98,8 +98,8 @@ navigate("/trips/123")
               )}
             </div>
             <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
-            com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e 
-              <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a></p>
+            com nossos <a className="text-zinc-300" href="#">termos de uso</a> e 
+              <a className="text-zinc-300" href="#"> políticas de privacidade.</a></p>
         </div>
 
         {isGuestsModalOpen && (
