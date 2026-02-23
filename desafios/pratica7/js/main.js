908654bc7474c42c8761216ee966956fad5f95f1
Diego <diegoramos3709@gmail.com>
2026-02-22 21:23:37 -0300

    Update main.js

--- desafios/pratica7/js/main.js
+++ desafios/pratica7/js/main.js
@@ -1,5 +1,28 @@
-document.addEventListener("click", (e) => {
-  if (e.target.classlist.contains("include-goals")) {
-    console.log("including");
+const input = document.getElementById("include-goals-input");
+const button = document.getElementById("include-goals-button");
+const display = document.getElementById("tasksArray");
+
+const storageTask = localStorage.getItem("tasks");
+
+let tasksArray = [];
+
+console.log(storageTask);
+button.addEventListener("click", function () {
+  const inputValue = input.value;
+
+  if (inputValue || tasksArray.length > 0) {
+    tasksArray = [...tasksArray, inputValue];
+    display.innerHTML = "";
+
+    tasksArray.forEach((task) => {
+      const newTask = document.createElement("li");
+      newTask.textContent = task;
+      display.appendChild(newTask);
+      tasksArray = [...tasksArray, newTask];
+    });
+    input.value = "";
+    localStorage.setItem("tasks", JSON.stringify(tasksArray));
+  } else {
+    display.innerText = "Digite a tarefa que deseja adicionar";
   }
 });
